a0ce530cc5889f10d3f1bba2abdb806dc6c4b227
Robert Owens <bobdosgames@gmail.com>
2020-10-31 23:22:07 -0500

    First revision of postfix installer

--- deploy.py
+++ deploy.py
@@ -1,18 +1,60 @@
 import os
 import socket
 import platform
+import sys
+import argparse
+import re
 
-# Read config file here
+parser = argparse.ArgumentParser()
 
-# Identify Hostname and Platform
+parser.add_argument(
+    "--hostname",
+    dest="hostname",
+    type=str,
+    default="auto",
+    help="Sets the Hostname or FQDN to be used in configuration of the mail server.",
+)
 
-HOSTNAME = socket.gethostname()
+args = parser.parse_args()
 
-print(HOSTNAME)
-
-PLATFORM = platform.system()
-
-print(PLATFORM)
-
-if "windows" in PLATFORM.lower():
+# Check to make sure running on compatible operating system.
+if "windows" in platform.system().lower():
     print("Automail is inteded to run on Ubuntu. \n Sorry!")
+    sys.exit(1)
+
+# Get system hostname.
+if args.hostname is "auto":
+    hostname = os.popen("hostname -f").read()
+else:
+    hostname = args.hostname
+
+# Matches for a FQDN.
+# Regex Explained here https://regex101.com/r/uXHuOZ/3
+if re.search("^[^\.]+(\.[^\.]+)+$", hostname):
+    print(f"FQDN detected as {hostname} \n")
+else:
+    print(f"Hostname detected as {hostname} \n")
+    print(f"Please set a FQDN. \n")
+    sys.exit(1)
+
+# Update packages.
+try:
+    os.system("apt-get update -y")
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+# Preseed FQDN option for postfix install.
+os.system(f'echo "postfix	postfix/mailname string {hostname}" | debconf-set-selections')
+
+# Preseed Postfix to sending emails to other MTAs and receiving email from other MTAs.
+os.system(
+    "echo \"postfix postfix/main_mailer_type string 'Internet Site'\" | debconf-set-selections"
+)
+
+# Silent install postfix.
+try:
+    os.system("apt-get install -y postfix")
+except Exception as e:
+    print(e)
+    sys.exit(1)
